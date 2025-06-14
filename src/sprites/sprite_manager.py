48e78835809fa786fa069c17c26c17700faccc1c
aznoH333 <glentik23@gmail.com>
2025-06-14 14:52:03 +0200

    tile coloring

--- src/sprites/sprite_manager.py
+++ src/sprites/sprite_manager.py
@@ -30,9 +30,9 @@ class SpriteManager:
         
         surface = pygame.Surface((self.SPRITE_SCALE * self.GAME_ZOOM, self.SPRITE_SCALE * self.GAME_ZOOM))
 
-        surface.fill((255, 255, 255))
-        surface.blit(self.sprite_sheet, (0, 0), rect)
-        self.screen.blit(surface, (x * self.GAME_ZOOM, y * self.GAME_ZOOM),rect, special_flags=pygame.BLEND_RGBA_MULT)
+        surface.fill((255, 0, 255))
+        surface.blit(self.sprite_sheet, (0, 0), rect, special_flags=pygame.BLEND_RGBA_MULT)
+        self.screen.blit(surface, (x * self.GAME_ZOOM, y * self.GAME_ZOOM))
 
     def update_screen(self):
         self.screen.fill((0, 0, 0))
